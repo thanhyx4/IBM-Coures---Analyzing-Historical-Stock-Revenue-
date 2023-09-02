4e1972655c746b4ee6101052318b8e1d23e4fa9a
Thanh <58739801+thanhyx4@users.noreply.github.com>
2023-09-02 23:58:55 +0700

    Update DAG.py

--- IBM_DE_Course/DAG.py
+++ IBM_DE_Course/DAG.py
@@ -1,1 +1,21 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+import datetime as dt
+
+default_args = {
+	'owner' : 'me',
+	'start_date' : dt.datetime(2023,08,22),
+    'email' : 'abc@gmail.com',
+    'email_on_failure' : True,
+    'email_on_retry' : True,
+	'retries' : 1
+	'retry_delay'  : dt.timedelta(minutes = 5),
+}
+dag = DAG ('ETL_toll_data', description='Apache Airflow Final Assignment', 
+default_args = default_args, schedule_interval = dt.timedelta(hours = 24) )
+
+task_1 = BashOperator ( task_id = ‘ abc’, 
+			bash_command = ‘ echo \’greetings. The date and time are \’ ‘,
+			dag = dag, )
 
