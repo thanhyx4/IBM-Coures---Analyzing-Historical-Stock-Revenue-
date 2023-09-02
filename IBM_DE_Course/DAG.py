a339b66450950065095169d116575a3feaf72e5b
Thanh <58739801+thanhyx4@users.noreply.github.com>
2023-09-03 00:11:41 +0700

    Update DAG.py

--- IBM_DE_Course/DAG.py
+++ IBM_DE_Course/DAG.py
@@ -5,17 +5,18 @@ import datetime as dt
 
 default_args = {
 	'owner' : 'me',
-	'start_date' : dt.datetime(2023,08,22),
+	'start_date' : dt.datetime(2023,9,3),
     'email' : 'abc@gmail.com',
     'email_on_failure' : True,
     'email_on_retry' : True,
-	'retries' : 1
+	'retries' : 1,
 	'retry_delay'  : dt.timedelta(minutes = 5),
 }
 dag = DAG ('ETL_toll_data', description='Apache Airflow Final Assignment', 
 default_args = default_args, schedule_interval = dt.timedelta(hours = 24) )
 
-task_1 = BashOperator ( task_id = ‘ abc’, 
-			bash_command = ‘ echo \’greetings. The date and time are \’ ‘,
+task_1 = BashOperator ( task_id = 'anc', 
+			bash_command = 'unzip tolldata.tgz',
 			dag = dag, )
 
+task_1
